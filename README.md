4de38967b8195c66fb7492e0657027d5d711ab92
a-slide <aleg@ebi.ac.uk>
2020-10-27 10:49:37 +0000

    Conditional deployment on tag full deploy test

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# versipy v0.2
+# versipy v0.2.dev1
 
 ![versipy](pictures/versipy.png)
 
--- versipy/__init__.py
+++ versipy/__init__.py
@@ -1,5 +1,5 @@
 name = "versipy"
-version = "0.2"
+version = "0.2.dev1"
 description = "Versatile version and medatada managment across the python packaging ecosystem, integrated with git"
 url = "https://github.com/a-slide/versipy"
 licence = "GPLv3"
